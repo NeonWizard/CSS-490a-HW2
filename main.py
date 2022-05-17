4f7df5c1aac6c885f40b012c47981f9198ef1894
Spooky <averagewizard13@gmail.com>
2022-05-17 00:56:11 -0700

    Working model training

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import adam_v2
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 import numpy as np
 
 # -- load images
@@ -58,10 +59,22 @@ model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
 model.add(Flatten())
 model.add(Dense(units=4096,activation="relu"))
 model.add(Dense(units=4096,activation="relu"))
-model.add(Dense(units=2, activation="softmax"))
+model.add(Dense(units=3, activation="softmax"))
 
 # -- Compile model
 opt = adam_v2.Adam(learning_rate=0.001)
 model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
 model.summary()
+
+# --
+checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=20, verbose=1, mode='auto')
+hist = model.fit(
+  train_data,
+  # steps_per_epoch=100,
+  validation_data=test_data,
+  # validation_steps=10,
+  epochs=100,
+  batch_size=8,
+  callbacks=[checkpoint,early])
