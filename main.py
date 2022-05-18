0ad3037c57bc6302f2e89d0c3684296c98281645
Spooky <averagewizard13@gmail.com>
2022-05-18 00:43:50 -0700

    Plot acc/loss graph and run prediction from saved model

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import keras, os
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import adam_v2
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.preprocessing import image
 import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+matplotlib.use('Agg')
 
 # -- load images
 id_generator = ImageDataGenerator()
@@ -67,9 +71,10 @@ model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics
 
 model.summary()
 
-# --
+# -- Model fitting
 checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=20, verbose=1, mode='auto')
+
 hist = model.fit(
   train_data,
   # steps_per_epoch=100,
@@ -78,3 +83,24 @@ hist = model.fit(
   epochs=100,
   batch_size=8,
   callbacks=[checkpoint,early])
+
+# # -- Evaluation
+plt.plot(hist.history["accuracy"])
+plt.plot(hist.history['val_accuracy'])
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title("Model Accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("Epoch")
+plt.legend(["Accuracy","Validation Accuracy","Loss","Validation Loss"])
+plt.savefig("acc_loss.png")
+
+# -- testing
+img = image.load_img("data/validate/paper1.png",target_size=(224,224))
+img = np.asarray(img)
+img = np.expand_dims(img, axis=0)
+saved_model = load_model("vgg16_1.h5")
+prediction_ind = saved_model.predict(img).argmax(axis=-1)
+prediction = np.array(["paper", "rock", "scissors"])[prediction_ind][0]
+
+print("Prediction: " + prediction)
