106cd4d8918a4e08923ac52c5e4ae7c0c900dd64
Spooky <averagewizard13@gmail.com>
2022-05-22 21:45:10 -0700

    Add evaluation metrics

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import adam_v2
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.preprocessing import image
+from keras import backend as K
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
@@ -65,26 +66,45 @@ model.add(Dense(units=4096,activation="relu"))
 model.add(Dense(units=4096,activation="relu"))
 model.add(Dense(units=3, activation="softmax"))
 
+# -- Evaluation metrics
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
 # -- Compile model
 opt = adam_v2.Adam(learning_rate=0.0001)
-model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
+model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy', f1_m, precision_m, recall_m])
 
 model.summary()
 
 # -- Model fitting
 checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
-early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=20, verbose=1, mode='auto')
+early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=1, mode='auto')
 
 hist = model.fit(
   train_data,
   # steps_per_epoch=100,
   validation_data=test_data,
   # validation_steps=10,
-  epochs=100,
+  epochs=10,
   batch_size=8,
   callbacks=[checkpoint,early])
 
 # # -- Evaluation
+# Accuracy over loss
 plt.plot(hist.history["accuracy"])
 plt.plot(hist.history['val_accuracy'])
 plt.plot(hist.history['loss'])
@@ -96,11 +116,31 @@ plt.legend(["Accuracy","Validation Accuracy","Loss","Validation Loss"])
 plt.savefig("acc_loss.png")
 
 # -- testing
-img = image.load_img("data/validate/paper1.png",target_size=(224,224))
+saved_model = load_model("vgg16_1.h5", custom_objects={
+  "f1_m": f1_m,
+  "precision_m": precision_m,
+  "recall_m": recall_m
+})
+
+loss, acc, f1_score, precision, recall = saved_model.evaluate(test_data)
+print()
+print("-- Evaluation on testing dataset --")
+print("Loss: {}\nAcc: {}\nF1 Score: {}\nPrecision: {}\nRecall: {}".format(
+  round(loss*100, 3),
+  round(acc*100, 3),
+  round(f1_score*100, 3),
+  round(precision*100, 3),
+  round(recall*100, 3),
+))
+print()
+
+img = image.load_img("data/validate/rock1.png", target_size=(224,224))
 img = np.asarray(img)
 img = np.expand_dims(img, axis=0)
-saved_model = load_model("vgg16_1.h5")
+
 prediction_ind = saved_model.predict(img).argmax(axis=-1)
 prediction = np.array(["paper", "rock", "scissors"])[prediction_ind][0]
 
+print()
 print("Prediction: " + prediction)
+print()
